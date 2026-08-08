583c68bab808c66752996f8eb3d110a0eb83b74c
GVN2307 <veeranarayana1gorantla@gmail.com>
2026-08-08 23:52:00 +0530

    Add project skeleton: data loader, run script, placeholder workloads

---
+++ harness/run_all.sh
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env bash
+set -e
+echo  Running benchmark...
+# Placeholder: call data loader
+python data_loaders\load_dataset.py
+# Placeholder: execute queries
+echo \Benchmark complete.\
